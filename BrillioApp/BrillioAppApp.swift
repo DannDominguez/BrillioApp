484a1fdc3fba33ffb62bc5c554e445cdb3b9e770
Daniela Ciciliano <dann@Danielas-MacBook-Air.local>
2024-04-16 14:15:03 -0600

    first commit

---
+++ BrillioApp/BrillioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrillioAppApp.swift
+//  BrillioApp
+//
+//  Created by Daniela Ciciliano on 16/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BrillioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
